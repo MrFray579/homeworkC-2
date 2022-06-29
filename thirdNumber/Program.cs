2c9b5e9847f008716a9438a6784fcc0874000557
Fray <kozar20000@mail.ru>
2022-06-29 21:33:48 +0300

    thirdNumber

---
+++ thirdNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = new Random().Next(1, 999);
+Console.WriteLine(A);
+int B = A % 10;
+
+ if (A >= 100)
+    {
+        Console.WriteLine(B);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+
+
+   
+
+
