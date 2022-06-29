33215cce69b8b4fce4bf0f62f78b083d769fee3e
Fray <kozar20000@mail.ru>
2022-06-29 22:26:39 +0300

    holiday

---
+++ holiday/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int a = Convert.ToInt32(Console.ReadLine());
+string[] weekDays = new string[] {"Mon", "Tue", "Web", "Thu", "Fri", "Sat", "Sun"};
+
+if (a <= 5)
+Console.WriteLine(weekDays[a - 1] + " No");
+else
+Console.WriteLine(weekDays[a - 1] + " Yes");
+
+
+/*Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+        Console.WriteLine("Mon no");
+        break;
+    case 2:
+        Console.WriteLine("Tue no");
+        break;
+    case 3:
+        Console.WriteLine("Web no");
+        break;
+    case 4:
+        Console.WriteLine("Thu no");
+        break;
+    case 5:
+        Console.WriteLine("Fri no");
+        break;
+    case 6:
+        Console.WriteLine("Sat yes");
+        break;
+    case 7:
+        Console.WriteLine("Sun yes");
+        break;
+    default:
+        Console.WriteLine("Введите другое число!");
+        break;
+}*/
