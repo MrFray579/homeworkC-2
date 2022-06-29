4a07dd9aba54e014d7d800b422e65cbc1f8a13dd
Fray <kozar20000@mail.ru>
2022-06-29 20:54:44 +0300

    secondNumber

---
+++ secondNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿void randomNumbersCompare (int start, int finish)
+{
+    int A = new Random().Next(start, finish);
+    Console.WriteLine(A);
+    int B = (A / 10) % 10;
+    Console.WriteLine(B);
+    
+}
+
+randomNumbersCompare (100, 999);
